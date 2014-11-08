46afe2463259ba9d81101bed739d00f6093f6d83
Bruce Mitchener <bruce.mitchener@gmail.com>
2014-11-08 08:21:02 +0700

    Add head-only formula for melange.

---
+++ melange.rb
@@ -0,0 +1,16 @@
+require "formula"
+
+class Melange < Formula
+  homepage "http://opendylan.org/documentation/melange"
+  head "https://github.com/dylan-lang/melange.git"
+
+  depends_on "opendylan" => :build
+
+  def install
+    system "make", "install", "DESTDIR=#{prefix}"
+  end
+
+  test do
+    system "false"
+  end
+end
